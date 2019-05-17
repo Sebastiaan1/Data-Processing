02a75bf49717a241baab7c9752e0b173b180ff2c
Sebastiaan1 <46788524+Sebastiaan1@users.noreply.github.com>
2019-05-17 21:08:11 +0200

    Delete reizigerDAO.java

--- reizigerDAO.java
+++
@@ -1,9 +0,0 @@
-package opdracht1_dao;
-import java.util.List;
-
-public interface reizigerDAO {
-	   public List<reiziger> getAllReizigers();
-	   public reiziger getReiziger(int reizigerID);
-	   public void updateReiziger(reiziger reiziger);
-	   public void deleteReiziger(reiziger reiziger);
-}

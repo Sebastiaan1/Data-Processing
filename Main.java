06e25b87dcafbc7a643ee91cb1ed56ff435a8f3e
Sebastiaan1 <46788524+Sebastiaan1@users.noreply.github.com>
2019-05-22 20:38:04 +0200

    Opdracht P2 toegevoegd

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 // Sebastiaan Jansen
 // Klas V1C
 
-//Opdracht P1
+//Opdracht P2
 
 
 package reiziger;
@@ -9,12 +9,15 @@ package reiziger;
 import reiziger.Reiziger;
 import reiziger.ReizigerDAO;
 import reiziger.ReizigerDAOimplement;
+import reiziger.OracleBaseDAO;
+import java.sql.*;
 
 public class Main {
-	   public static void main(String[] args) {
+	   public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+		   
 	      ReizigerDAO reizigerdao = new ReizigerDAOimplement();
 
-	      //print all reizigers
+	      //Print alle reizigers.
 	      for (Reiziger reiziger : reizigerdao.findAll()) {
 	         System.out.println("Reiziger:"
 	         		+ "\n	Naam : " + reiziger.getNaam()
@@ -23,12 +26,12 @@ public class Main {
 	      }
 
 
-	      //update reiziger
+	      //Update de eerste reiziger.
 	      Reiziger reiziger1 = reizigerdao.findAll().get(0);
 	      System.out.println(reiziger1.getGbdatum());
 	      reizigerdao.update(reiziger1);
 
-	      //get the reiziger
+	      //Print alle reizigers. (na update)
 	      for (Reiziger reiziger2 : reizigerdao.findAll()) {
 	    	  System.out.println("Reiziger:"
 		         		+ "\n	Naam : " + reiziger2.getNaam()
@@ -36,17 +39,30 @@ public class Main {
 		         		+ "\n");
 		      }	
 	      
-	      //delete reiziger
+	      //Delete de eerste reiziger.
 	      Reiziger reiziger3 = reizigerdao.findAll().get(0);
 	      reizigerdao.delete(reiziger3);
 
-	      //get the reiziger
+	      //Print alle reizigers. (na delete)
 	      for (Reiziger reiziger4 : reizigerdao.findAll()) {
 	    	  System.out.println("Reiziger:"
 		         		+ "\n	Naam : " + reiziger4.getNaam()
 		         		+ "\n	Geboortedatum : " + reiziger4.getGbdatum()
 		         		+ "\n");
-		      }	
+		      }
+	      
+	      System.out.println("-----------------------------------------");
+	      System.out.println("-----------------------------------------");
+	      System.out.println("-----------------------------------------");
+	      
+	      //Instantieer een nieuwe OracleBase.
+		  OracleBaseDAO base = new OracleBaseDAO();
+		  //Maak een connectie met de database.
+	      Connection conn = base.getConnection();
+	      //Laat alle OV kaarten zien.
+	      base.showAllOV(conn);
+	      //Sluit de connectie.
+	      base.closeConnection(conn);
 	   }	
 }
 	
---
+++ OVchipkaart.java
@@ -0,0 +1,61 @@
+package reiziger;
+
+import java.sql.Date;
+
+public class OVchipkaart {
+	private int kaartNummer;
+	private Date geldigTot;
+	private int klasse;
+	private double saldo;
+	private Date gbDatum;
+	
+	public OVchipkaart(int kaartNummer, Date geldigTot, int klasse, double saldo, Reiziger reiziger){
+		this.kaartNummer = kaartNummer;
+		this.geldigTot = geldigTot;
+		this.klasse = klasse;
+		this.saldo = saldo;
+		this.gbDatum = reiziger.getGbdatum();
+	}
+
+	public int getKaartNummer() {
+		return kaartNummer;
+	}
+
+	public Date getGeldigTot() {
+		return geldigTot;
+	}
+
+	public int getKlasse() {
+		return klasse;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public Date getGbDatum() {
+		return gbDatum;
+	}
+
+	public void setKaartNummer(int kaartNummer) {
+		this.kaartNummer = kaartNummer;
+	}
+
+	public void setGeldigTot(Date geldigTot) {
+		this.geldigTot = geldigTot;
+	}
+
+	public void setKlasse(int klasse) {
+		this.klasse = klasse;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public void setGbDatum(Date gbDatum) {
+		this.gbDatum = gbDatum;
+	}
+
+}
+

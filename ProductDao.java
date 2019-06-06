b19f1e93c372c0941e7a3f564c5b6632c2efdcb2
Sebastiaan1 <46788524+Sebastiaan1@users.noreply.github.com>
2019-06-06 17:07:18 +0200

    Delete ProductDao.java

--- ProductDao.java
+++
@@ -1,9 +0,0 @@
-package dao_implementatie;
-
-import java.util.List;
-
-public interface ProductDao {
-	public Product save(Product product);
-	public Product update(Product product);
-	public boolean delete(Product product);
-}
